a5090a90d6d728d28ab7b9f243dfc0b4383f8137
henryliu <17600047539@163.com>
2024-03-09 00:13:59 +0800

    update

--- packages/objects-package/scripts/mint.ts
+++ packages/objects-package/scripts/mint.ts
@@ -7,7 +7,7 @@ import {OBJECT} from "./package";
 const main =  async () =>{
 
 
-  const package_address = '0xe3b146d91c993afd7e22b3541fd41b5b479f4f2c2b2fd695dd05484f7079fb86'
+  const package_address = '0x5681df2356f48c1c29bc28151d83cc179a600de2fc63524f5ec8f6a5a4b864ee'
 
   const suiClient = new SuiClient({ url: getFullnodeUrl('testnet') });
   const keypair = Ed25519Keypair.fromSecretKey(Buffer.from("0bc2bd8d2135c9c0ea18d56cb0b021788e79c7fdf3435c455a049ee92c532a57", 'hex'))
@@ -16,7 +16,7 @@ const main =  async () =>{
   txb.setGasBudget(5000000000)
 
 
-  const descriptor = txb.pure('0x3ceee5cf8635f2ada99fccb300b952a01ffcfb34a033f82b429c0cd16503686b')
+  const descriptor = txb.pure('0xe8432e43ab5f226c4ef66730c79312935f79b1afa1e87640af7df175624122d9')
 
 
   txb.moveCall(
