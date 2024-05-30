75e1f878fc3d206468e777f608e0e09ebde0d764
vladilen11 <vladilenaksana@163.com>
2024-05-31 00:22:46 +0800

    update config

--- apps/web/app/chain/config.ts
+++ apps/web/app/chain/config.ts
@@ -3,10 +3,10 @@ type NetworkType = "testnet" | "mainnet" | "devnet" | "localnet";
 const NETWORK: NetworkType = "devnet";
 
 const PACKAGE_ID =
-  "0x92ef84155ccd3b5f377eff8b5c50d046a562bd9e0520837346173bf7c4f3a357";
+  "0x4399a8a89a93ce6a8e4b3b9ae0fcee43470dda562a5c8f7a13d606752a5ef0d0";
 
 const TREASURE_OBJECT_ADDRESS =
-  "0x92ef84155ccd3b5f377eff8b5c50d046a562bd9e0520837346173bf7c4f3a357::object::OBJECT";
+  "0x4399a8a89a93ce6a8e4b3b9ae0fcee43470dda562a5c8f7a13d606752a5ef0d0::object::OBJECT";
 
 const TREASURE_ADDRESS =
   "0x59a5fbf2c56da3a4a2ac761f062cb0e8ed6c6cb1812136178cf2321586736cc7";
@@ -15,14 +15,14 @@ const OBJECT_ADDRESS =
   "0xbfefbf1ee21892c1938ab9ab3ade4222c9970e1defc7d3416dd37a40c35462e5";
 
 const OBJECT_TYPE =
-  "0x92ef84155ccd3b5f377eff8b5c50d046a562bd9e0520837346173bf7c4f3a357::objects_seeder::Objects";
+  "0x4399a8a89a93ce6a8e4b3b9ae0fcee43470dda562a5c8f7a13d606752a5ef0d0::objects_seeder::Objects";
 
 const ObjectsDescriptor =
-  "0x37d35f1e115eaf4276b071e57ec907c4b4d1c33b52b4401b8172b178c0d36f7a";
+  "0xbba50b6cef24dca3658e709c91629cd71248aa7b1678dfb03f230afcc27151d1";
 const AuctionManager =
-  "0x34e78212f520c012927c3f9d27a10657a9cd92b1ef97647db0193449a06deca5";
+  "0x1271bfea4b67ff408bd825bfcbe7f4c41c5e7997e8bde107729bc27b64a29101";
 const GovManager =
-  "0x2457fcb1e4bb6827d74ecfdc65af8de58ecdbb28b99568d601f82431e5e5795d";
+  "0x5c417d20c0042dea4df07abe4179e1f23aef7ce549a9e661da9267f5dd8de046";
 
 export {
   NETWORK,
